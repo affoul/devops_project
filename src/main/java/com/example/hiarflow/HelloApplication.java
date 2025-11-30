0e453dcf26e53209b629370f23ae04c695dbab21
“[fares <[faresderouich4@gmail.com]>
2025-11-30 18:07:08 +0100

    Ajout Jenkinsfile pour CI/CD

--- src/main/java/com/example/hiarflow/HelloApplication.java
+++ src/main/java/com/example/hiarflow/HelloApplication.java
@@ -7,7 +7,48 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+            steps {
+                echo 'Récupération du code depuis GitHub'
+                git branch: 'main', url: 'https://github.com/affoul/devops_project.git'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo 'Compilation du projet avec Maven'
+                sh 'mvn clean install'
+            }
+        }
+
+        stage('Tests') {
+            steps {
+                echo 'Aucun test JUnit pour l’instant'
+                // Si tu ajoutes des tests plus tard, tu peux activer :
+                // sh 'mvn test'
+            }
+        }
+
+        stage('SAST') {
+            steps {
+                echo 'Analyse SonarQube (optionnel pour le mini-projet)'
+                // sh 'mvn sonar:sonar -Dsonar.projectKey=devops_project ...'
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                echo 'Déploiement du projet (copie jar/war sur Tomcat)'
+                // sh 'cp target/devops_project-1.0-SNAPSHOT.jar /chemin/tomcat/webapps/'
+            }
+        }
+    }
+}
+HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login.fxml"));
